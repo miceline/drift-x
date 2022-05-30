2ac6ab534450bddcac255ce6a54f67c09f342bd8
miceline <miceline@users.noreply.github.com>
2022-05-29 23:32:32 -0400

    deploy

---
+++ precache-manifest.61bdbb1d4ac391c89679b418b9bceb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2def9c821b3e008e76",
+    "url": "/drift-x/css/app.12e8f000.css"
+  },
+  {
+    "revision": "7168374307972f1b2568",
+    "url": "/drift-x/css/chunk-vendors.4bc3d884.css"
+  },
+  {
+    "revision": "e134d602b0b14d4c47facc773ae156aa",
+    "url": "/drift-x/index.html"
+  },
+  {
+    "revision": "2e2def9c821b3e008e76",
+    "url": "/drift-x/js/app.7a3f9245.js"
+  },
+  {
+    "revision": "7168374307972f1b2568",
+    "url": "/drift-x/js/chunk-vendors.13de7a1d.js"
+  },
+  {
+    "revision": "4641eeb71615cebb8c43f1a938a41ca4",
+    "url": "/drift-x/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/drift-x/robots.txt"
+  }
+]);
